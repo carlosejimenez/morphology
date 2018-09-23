54bdc7fbf0538fbea2a488ac10a56db14ff26ff0
Carlos Jiménez-Bravo <cjsaltlake@gmail.com>
2018-09-23 15:08:19 -0600

    Remove inline comments and unify block style.

--- morphology.py
+++ morphology.py
@@ -8,6 +8,7 @@ import sys
 def get_definition(word, entry, source):
     if len(entry) > 1:
         root = entry[2]
+        
     else:
         root = word
 
@@ -20,79 +21,63 @@ def morphologies(word, rules):
     for rule in rules:
         if rule[0] == 'SUFFIX':
             pattern = rule[1] + '$'
+            
         else:
             pattern = '^' + rule[1]
+            
         match = re.search(pattern, word)
+        
         if match:
-
-            # i and f are used to cut out the suffix or prefix from the word and parse its stem.
-            # stem = word[:i] + replacement + word[f:]
             i, f = map(int, match.span())
             if rule[2] == '-':
-
-                # Recursively check stems for successful parses.
                 sub_parses = morphologies(word[:i] + word[f:], rules)
                 if len(sub_parses) > 0:
                     for sub_parse in sub_parses:
-
-                        # If the successful sub-parse matches the current grammar rule, it is successful overall.
                         if re.search(rule[3], sub_parse[len(word) - f + i:]):
                             sub_parse = sub_parse.replace(rule[3], rule[5])
                             parses.add(sub_parse.replace(word[:i] + word[f:], word))
-
-                # If the stem of our word is in the dictionary, we can create a new parse.
+                            
                 if (word[:i] + word[f:]).lower() in internal_dict:
                     for definition in internal_dict[(word[:i] + word[f:]).lower()]:
-
-                        # If the POS of the stem in the dictionary matches the current grammar rule, it's successful.
                         if definition[0] == rule[3]:
                             parse = word + ' ' + rule[5] + ' ROOT=' + word[:i] + word[f:] + ' SOURCE=morphology'
                             parses.add(parse)
+                            
             else:
-                # Recursively check stems for successful parses.
                 sub_parses = morphologies(word[:i] + rule[2] + word[f:], rules)
                 if len(sub_parses) > 0:
                     for sub_parse in sub_parses:
-
-                        # If the successful sub-parse matches the current grammar rule, it is successful overall.
                         if re.search(rule[3], sub_parse[len(word) + len(rule[2]) - f + i:]):
                             sub_parse = sub_parse.replace(rule[3], rule[5])
                             parses.add(sub_parse.replace(word[:i] + rule[2] + word[f:], word))
 
-                # If the stem of our word is in the dictionary, we can create a new parse.
                 if (word[:i] + rule[2] + word[f:]).lower() in internal_dict:
                     for definition in internal_dict[(word[:i] + rule[2] + word[f:]).lower()]:
 
-                        # If the POS of the stem in the dictionary matches the current grammar rule, it's successful.
                         if definition[0] == rule[3]:
                             parse = word + ' ' + rule[5] + ' ROOT=' + word[:i] + rule[2] + word[ f:] + ' SOURCE=morphology'
                             parses.add(parse)
+                            
         else:
             continue
 
     return parses
 
-
-# Copy command line arguments into relevant file objects.
 dictionary_file = open(sys.argv[1], "rt")
 rules_file = open(sys.argv[2], 'rt')
 test_file = open(sys.argv[3], 'rt')
 
-# internal_dict will hold data in a convenient form from the dictiary file.
 internal_dict = {}
 
-# parses will hold all of the successful parses.
 parses = {}
 
-# Extract all data from files into useful data structures.
 line_list = [line.strip().split(' ') for line in dictionary_file.readlines()]
 dictionary_file.close()
 for line in line_list:
 
-    # If the word is in the internal_dict, we want to add the new definition to the list.
-    # Otherwise, we make a new list of definitions.
     if line[0].lower() in internal_dict:
         internal_dict[line[0].lower()].append(line[1::])
+        
     else:
         internal_dict[line[0].lower()] = [line[1::]]
 
@@ -106,9 +91,9 @@ for word in tests:
         for each_definition in internal_dict[word.lower()]:
             definition = get_definition(word.lower(), each_definition, 'dictionary').split(' ')
 
-            # Each word will be associated with a list of successful parses.
             if definition[0] in parses:
                 parses[definition[0]].append(definition[1:])
+                
             else:
                 parses[definition[0]] = [definition[1:]]
 
@@ -119,13 +104,14 @@ for word in tests:
                 forms_list = form.split(' ')
                 if forms_list[0] in parses:
                     parses[forms_list[0]].append(forms_list[1:])
+                    
                 else:
                     parses[forms_list[0]] = [forms_list[1:]]
+                    
         else:
             default_parse = [['noun', 'ROOT=' + word.lower(), 'SOURCE=default']]
             parses[word.lower()] = default_parse
 
-# Finally, we output the successful parses grouped by distinct entry-words.
 for word in parses:
     for parse in parses[word]:
         print(word, end='')
